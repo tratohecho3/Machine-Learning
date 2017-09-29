3a58ec89f5b823faddae277f527368e6629b31a7
César Colina <tratohecho3@gmail.com>
2017-09-29 13:08:27 -0400

    Add files via upload

--- linear_3_features.py
+++ linear_3_features.py
@@ -18,7 +18,7 @@ def ten_percent(data,output):
 	output_training = output[ten_percent:]
 	output_test = output[:ten_percent]
 
-	return training_set,test_set,output_training,output_test
+	return np.array(training_set),np.array(test_set),output_training,output_test
 def changeHA(x):
 	final = x * 627.509469
 
@@ -42,6 +42,7 @@ def extract_mol(adl):
 		    #print("  Energies:    ", E,"\n")
 		    
 		i += 1
+	
 	return X,E
 def combinations(coordinates):
 	training_set = []
@@ -84,19 +85,17 @@ def other_function():
 		output_data.append(f(training_data[i][0],training_data[i][1]))
 	
 	return training_data,output_data
-def f(x,y):
-	z = y*math.sin(x)
-	return z
+
 def solo_aqui(x):
 	x_train = []
 	for i in range(len(x)):
 		x_train.append(x[i][0])
-	return np.array(x_train)
+	return np.float32(x_train)
 def solo_aqui2(x):
 	x_train = []
 	for i in range(len(x)):
 		x_train.append(x[i][1])
-	return np.array(x_train)
+	return np.float32(x_train)
 def dividir(x):
 	vector1 = []
 	vector2 = []
@@ -111,63 +110,61 @@ def solo_aqui3(x):
 	x_train = []
 	for i in range(len(x)):
 		x_train.append(x[i][2])
-	return np.array(x_train)
-hdf5file = 'gdb11_S01_06r.h5'
+	return np.float32(x_train)
+def function_3_variable():
+	data = np.random.random_sample((1500,3))
+	output = []
+	for i in range(len(data)):
+		output.append(f(data[i][0],data[i][1],data[i][2]))
+	return data,np.array(output)
+def f(x,y,z):
+	z = ((x**2)+(y**3)+ 3*z)
+	return z
+
+hdf5file = 'ani_gdb_s01.h5'
 adl = pya.anidataloader(hdf5file)
 coordinates,output_training = extract_mol(adl)
 training_set = combinations(coordinates)
 adl.cleanup()
 
-#INPUT #2
-#training_set,output_training = other_function()
-
-"""
-#INPUT #3
-training_set = [[3.3], [4.4], [5.5], [6.71], [6.93], [4.168], 
-                    [9.779], [6.182], [7.59], [2.167], [7.042], 
-                    [10.791], [5.313], [7.997], [3.1]]
-output_training = [[1.7], [2.76], [2.09], [3.19], [1.694], [1.573], 
-                    [3.366], [2.596], [2.53], [1.221], [2.827], 
-                    [3.465], [1.65], [2.904], [1.3]]
-"""
-#INPUT #4
-#training_set = np.random.random_sample((15,1))
-#output_training =np.random.random_sample((15,1))
-
-#INPUT #5
-#training_set = rellenar(training_set)
-#training_set = vaciar(training_set)
-
-#INPUT #6
-#training_set,output_training = cos()
-
 #DATA SET
+
+training_set,output_training  = function_3_variable()
 training_set,test_set,output_training,output_test = ten_percent(training_set,output_training)
 
 
+"""
+print(len(training_set),len(test_set),len(output_training),len(output_test))
+training_set = training_set[0:50]
+test_set = test_set[0:5]
+output_training = output_training[0:50]
+output_test = output_test[0:5]
+"""
+tf.nn.l2_normalize(training_set, 0, epsilon=1e-12)
+tf.nn.l2_normalize(test_set, 0, epsilon=1e-12)
 
 feature_column_1 = tf.feature_column.numeric_column("x1")
 feature_column_2 = tf.feature_column.numeric_column("x2")
 feature_column_3 = tf.feature_column.numeric_column("x3")
 
 
-
-#estimator = tf.estimator.LinearRegressor(feature_columns=feature_columns)	
-estimator = tf.estimator.LinearRegressor(feature_columns=[feature_column_1,feature_column_2,feature_column_3])	
-
+	
+#estimator = tf.estimator.LinearRegressor(feature_columns=[feature_column_1,feature_column_2,feature_column_3],optimizer=tf.train.GradientDescentOptimizer(0.0001))	
+#estimator = tf.estimator.DNNRegressor(feature_columns=[feature_column_1,feature_column_2,feature_column_3],optimizer=tf.train.GradientDescentOptimizer(0.01),hidden_units=[32])	
+estimator = tf.estimator.DNNRegressor(feature_columns=[feature_column_1,feature_column_2,feature_column_3],optimizer=tf.train.AdamOptimizer(learning_rate=0.01,beta1=0.9,beta2=0.999,epsilon=1e-08),hidden_units=[32,32], activation_fn=tf.tanh)	
 
 x_train_1 = solo_aqui(training_set)
 x_train_2 = solo_aqui2(training_set)
 x_train_3 = solo_aqui3(training_set)
 
-y_train = np.array(output_training)
+y_train = np.float32(output_training)
 
 x_eval_1 = solo_aqui(test_set)
 x_eval_2 = solo_aqui2(test_set)
 x_eval_3 = solo_aqui3(test_set)
 
 
-y_eval = np.array(output_test)
+y_eval = np.float32(output_test)
 
 
 
@@ -178,7 +175,7 @@ train_input_fn = tf.estimator.inputs.numpy_input_fn(
     {"x1": x_train_1,"x2": x_train_2, "x3":x_train_3}, y_train, batch_size=4, num_epochs=1000, shuffle=False)
 
 eval_input_fn = tf.estimator.inputs.numpy_input_fn(
-    {"x1": x_eval_1,"x2": x_eval_2, "x3":x_eval_3}, y_eval, batch_size=4, num_epochs=1000, shuffle=False)
+    {"x1": x_eval_1,"x2": x_eval_2, "x3":x_eval_3}, y_eval, batch_size=4	, num_epochs=1000, shuffle=False)
 
 
 # We can invoke 1000 training steps by invoking the  method and passing the
@@ -201,7 +198,12 @@ final = []
 for i in range(len(y_eval)):
 	final.append(predicted_classes[i][0])
 
+print(y_eval.shape,np.array(final).shape)
+print(y_eval[0:35],"esta es la entrada")
+print("						")
+print(final[0:35])
 
+plt.plot(y_eval,y_eval)
 plt.scatter(y_eval,np.array(final))
 
 plt.show()
